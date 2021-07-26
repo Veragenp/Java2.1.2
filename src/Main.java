458071cb22f95bf1c8186ff2d84b4e17725877f8
Yury <yp@unotex.ru>
2021-07-26 14:07:08 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int start_balance = 100;
+        int refill = 2000;
+
+        int bonus_price = 100;
+        int bonus = (refill > 1000) ? refill/bonus_price : 0 ;
+        int final_balance = start_balance + refill + bonus;
+
+        System.out.println("Баланс на счете - " + final_balance + " руб.");
+        System.out.println("Бонусы - " + bonus + ("  руб."));
+    }
+}
